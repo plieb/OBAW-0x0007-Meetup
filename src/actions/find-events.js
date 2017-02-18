217ce6e8b9332e7e7f6d47d0c67bd622eb9571e2
PE Lieb <lieb.pe@gmail.com>
2017-02-18 18:10:41 +0100

    [ADD] cards for event search

--- src/actions/find-events.js
+++ src/actions/find-events.js
@@ -6,7 +6,7 @@ export default async function findEvents(res) {
   console.log('FIND EVENTS')
 
   const replies = []
-  const quickReplies = []
+  const cardsReplies = []
   const location = res.getMemory('location')
   console.log('======================================')
   console.log(location)
@@ -15,34 +15,30 @@ export default async function findEvents(res) {
     replies.push(formatter.formatMsg(`Looking for meetups near ${location.formatted}`))
     const response = await agent('GET', `https://api.meetup.com/find/events?key=${process.env.MEETUP_API_KEY}&lat=${location.lat}&long=${location.lng}`)
     const meetups = response.body
-    console.log('======================================')
-    console.log(meetups)
-    console.log('======================================')
-    /*replies.push(formatter.formatMsg(info))
-    if (filmAnswer.results[0].characters.length) {
-      const responseCharacter = await agent('GET', filmAnswer.results[0].characters[0])
-      const characterAnswer = responseCharacter.body
-      quickReplies.push({ name: characterAnswer.name, value: `Can I get information about ${characterAnswer.name}` })
+    if (meetups.length) {
+      const index = Math.floor((Math.random() * (meetups.length - 10)) + 1)
+      const meetupSliced = meetups.slice(index, index + 8)
+      console.log('======================================')
+      console.log(meetupSliced)
+      console.log('======================================')
+      meetupSliced.forEach(async (m) => {
+        console.log('======================================')
+        console.log(m)
+        console.log('======================================')
+        const responsePicture = await agent('GET', `https://api.meetup.com/${m.group.urlname}?key=${process.env.MEETUP_API_KEY}&lat=${location.lat}&long=${location.lng}`)
+        const picture = responsePicture.body
+        cardsReplies.push({
+          name: m.name,
+          city: m.venue.adress_1,
+          picture: picture.organizer.photo.photo_link,
+        })
+      })
+      replies.push(formatter.formatCardsReplies(cardsReplies))
+    } else {
+      replies.push(formatter.formatMsg(`Couldn't fint any meetups near ${location.formatted}`))
     }
-    if (filmAnswer.results[0].planets.length) {
-      const responsePlanet = await agent('GET', filmAnswer.results[0].planets[0])
-      const planetAnswer = responsePlanet.body
-      quickReplies.push({ name: planetAnswer.name, value: `Can I get information about ${planetAnswer.name}` })
-    }
-    if (filmAnswer.results[0].starships.length) {
-      const responseStarship = await agent('GET', filmAnswer.results[0].starships[0])
-      const starshipAnswer = responseStarship.body
-      quickReplies.push({ name: starshipAnswer.name, value: `Can I get information about ${starshipAnswer.name}` })
-    }
-    if (filmAnswer.results[0].vehicles.length) {
-      const responseVehicle = await agent('GET', filmAnswer.results[0].vehicles[0])
-      const vehicleAnswer = responseVehicle.body
-      quickReplies.push({ name: vehicleAnswer.name, value: `Can I get information about ${vehicleAnswer.name}` })
-    }
-    replies.push(formatter.formatQuickReplies(quickReplies, film))
   } else {
-    replies.push(formatter.formatMsg(`Sorry I couldn't find any information regarding ${film.value}`))
-  }*/
+    replies.push(formatter.formatMsg(res.reply()))
   }
   return replies
 }

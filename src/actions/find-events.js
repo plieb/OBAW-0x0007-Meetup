f36cf29de9615c0f596ca0b091ca9c995e850be6
PE Lieb <lieb.pe@gmail.com>
2017-02-18 19:49:33 +0100

    [ADD] asyn package

--- src/actions/find-events.js
+++ src/actions/find-events.js
@@ -1,56 +1,39 @@
 /* module imports */
 const agent = require('superagent-promise')(require('superagent'), Promise)
 const formatter = require('../formatter')
+const async = require('async')
 
-export default function findEvents(res) {
+export default async function findEvents(res) {
   console.log('FIND EVENTS')
 
-  return new Promise(async (resolve) => {
-    const replies = []
-    const cardsReplies = []
-    const location = res.getMemory('location')
-    if (location) {
-      replies.push(formatter.formatMsg(`Looking for meetups near ${location.formatted}`))
-      const response = await agent('GET', `https://api.meetup.com/find/events?key=${process.env.MEETUP_API_KEY}&lat=${location.lat}&long=${location.lng}`)
-      const meetups = response.body
-      if (meetups.length) {
-        const index = Math.floor((Math.random() * (meetups.length - 10)) + 1)
-        const meetupSliced = meetups.slice(index, index + 8)
-        const promises = meetupSliced.map(p => agent('GET', `https://api.meetup.com/${p.group.urlname}?key=${process.env.MEETUP_API_KEY}`))
-        Promise.all(promises).then((results) => {
-          console.log('======================================')
-          console.log(results)
-          console.log('======================================')
-          results.forEach((responsePicture, i) => {
-            const picture = responsePicture.body
-            const m = meetupSliced[i]
-            console.log('***************************************')
-            console.log(m)
-            console.log(picture.organizer.photo.photo_link)
-            console.log('***************************************')
-            cardsReplies.push({
-              name: m.name,
-              city: m.venue.city,
-              picture: picture.organizer.photo.photo_link,
-            })
-          })
-          console.log('======================================')
-          console.log(cardsReplies)
-          console.log('======================================')
-          replies.push(formatter.formatCardsReplies(cardsReplies))
-          resolve(replies)
-        }).catch((e) => {
-          console.log('======================================')
-          console.log(e)
-          console.log('======================================')
+  const replies = []
+  const cardsReplies = []
+  const location = res.getMemory('location')
+  if (location) {
+    replies.push(formatter.formatMsg(`Looking for meetups near ${location.formatted}`))
+    const response = await agent('GET', `https://api.meetup.com/find/events?key=${process.env.MEETUP_API_KEY}&lat=${location.lat}&long=${location.lng}`)
+    const meetups = response.body
+    if (meetups.length) {
+      const index = Math.floor((Math.random() * (meetups.length - 10)) + 1)
+      const meetupSliced = meetups.slice(index, index + 8)
+      async.each(meetupSliced, (m) => {
+        const responsePicture = agent('GET', `https://api.meetup.com/${m.group.urlname}?key=${process.env.MEETUP_API_KEY}`)
+        const picture = responsePicture.body
+        cardsReplies.push({
+          name: m.name,
+          city: m.venue.city,
+          picture: picture.organizer.photo.photo_link,
         })
-      } else {
-        replies.push(formatter.formatMsg(`Couldn't fint any meetups near ${location.formatted}`))
-        resolve(replies)
-      }
+      }, (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          replies.push(formatter.formatCardsReplies(cardsReplies))
+        }
+      })
     } else {
       replies.push(formatter.formatMsg(res.reply()))
-      resolve(replies)
     }
-  })
+  }
+  return replies
 }

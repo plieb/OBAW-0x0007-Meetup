12d137cf36351c0b8ca4497e81f3690494cff51c
PE Lieb <lieb.pe@gmail.com>
2017-02-19 21:19:26 +0100

    [ADD] to index register action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,16 @@
 const actions = {
   'find-events': require('./find-events'),
   'get-upcoming': require('./get-upcoming'),
+  'register': require('./register'),
 }
 
-export default async function handleAction(res, message) {
+export default async function handleAction(res, payload) {
   const currentAction = res.action && res.action.slug
   console.log(currentAction)
   let replies = []
   if (actions[currentAction]) {
     console.log('Enter action')
-    replies = await actions[currentAction].default(res)
+    replies = await actions[currentAction].default(res, payload)
   } else if (res.reply()) {
     replies.push({
       type: 'text',

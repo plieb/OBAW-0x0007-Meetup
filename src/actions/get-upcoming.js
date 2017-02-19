2ff4861e88a6422d05f427809136ae48b24036b2
PE Lieb <lieb.pe@gmail.com>
2017-02-19 19:48:18 +0100

    [FIX] check if empty

--- src/actions/get-upcoming.js
+++ src/actions/get-upcoming.js
@@ -12,15 +12,22 @@ export default async function getUpcoming(res) {
   const response = await agent('GET', `https://api.meetup.com/self/events?key=${process.env.MEETUP_API_KEY}&status=upcoming`)
   const meetups = response.body
   if (meetups.length) {
-    const m = meetups.slice(0, 8)
-    pictures[0] = await agent('GET', `https://api.meetup.com/${m[0].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
-    pictures[1] = await agent('GET', `https://api.meetup.com/${m[1].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
-    pictures[2] = await agent('GET', `https://api.meetup.com/${m[2].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
-    pictures[3] = await agent('GET', `https://api.meetup.com/${m[3].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
-    pictures[4] = await agent('GET', `https://api.meetup.com/${m[4].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
-    pictures[5] = await agent('GET', `https://api.meetup.com/${m[5].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
-    pictures[6] = await agent('GET', `https://api.meetup.com/${m[6].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
-    pictures[7] = await agent('GET', `https://api.meetup.com/${m[7].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
+    const m = meetups.slice(0, 5)
+    if (m[0]) {
+      pictures[0] = await agent('GET', `https://api.meetup.com/${m[0].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
+    }
+    if (m[1]) {
+      pictures[1] = await agent('GET', `https://api.meetup.com/${m[1].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
+    }
+    if (m[2]) {
+      pictures[2] = await agent('GET', `https://api.meetup.com/${m[2].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
+    }
+    if (m[3]) {
+      pictures[3] = await agent('GET', `https://api.meetup.com/${m[3].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
+    }
+    if (m[4]) {
+      pictures[4] = await agent('GET', `https://api.meetup.com/${m[4].group.urlname}?key=${process.env.MEETUP_API_KEY}`)
+    }
     pictures.forEach((p, i) => {
       const picture = p.body
       if (picture.group_photo) {
